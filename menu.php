5f436aedec2f35533ee1c0eb32d68ea549f98b5e
Jinsong Shi <qq1062787843@hotmail.com>
2016-11-07 17:43:49 +0800

    修复了一个谜一样的错误

--- menu.php
+++ menu.php
@@ -106,8 +106,7 @@ imageeffct();
  setupContents();
 lue();
 reHighlightCodeBlock();
-if ($('.ds-thread').length > 0) { if (typeof DUOSHUO !== 'undefined') DUOSHUO.EmbedThread('.ds-thread'); else $.getScript("http://www.ihewro.com/duoshuo/embedhw4.min.js
-"); }
+if ($('.ds-thread').length > 0) { if (typeof DUOSHUO !== 'undefined') DUOSHUO.EmbedThread('.ds-thread'); else $.getScript("http://www.ihewro.com/duoshuo/embedhw4.min.js"); }
 });
 
 </script>
